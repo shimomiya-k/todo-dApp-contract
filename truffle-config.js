05f12889bb6a51495d0d4369c362f45239912d37
Kazuma Shimomiya <shimomiya_@indent.co.jp>
2022-06-18 01:26:37 +0900

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { API_URL, MNEMONIC } = process.env;
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*",
+    },
+    matic: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: MNEMONIC,
+          },
+          url: API_URL,
+          chainId: 80001,
+        }),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      chainId: 80001,
+    },
+  },
+  contracts_directory: "./contracts",
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  db: {
+    enabled: false,
+  },
+};
